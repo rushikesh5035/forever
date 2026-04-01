428a553b949f63074f38dc49e00ee15a3bd03b34
Rushikesh Tele <155529923+rushikesh5035@users.noreply.github.com>
2026-04-01 15:47:39 +0530

    Refactor main.jsx to use basename for BrowserRouter and update vite.config.js to support dynamic base path based on mode

--- Admin/vite.config.js
+++ Admin/vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
+  base: mode === "production" ? "/admin/" : "/",
   server: { port: 5174 },
-});
+}));
